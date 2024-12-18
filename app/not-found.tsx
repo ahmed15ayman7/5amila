154815825125ddd7f96b4ee6e2b4cb31ec075e02
Ziad Eldeep <110999904+ZiadEldeep@users.noreply.github.com>
2024-12-18 23:43:57 +0200

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFound() {
     <div className="flex flex-col items-center w-full justify-center min-h-screen bg-white dark:bg-gray-900">
       <div className="text-center flex flex-col items-center">
         <h1 className="text-8xl font-extrabold text-gray-800 dark:text-gray-100 mb-4">
-          404
+          4004
         </h1>
         <h2 className="text-3xl font-semibold text-gray-700 dark:text-gray-300 mb-6">
           Page Not Found
